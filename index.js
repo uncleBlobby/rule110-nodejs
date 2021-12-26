067bc1459ce714822082db320a0c36554333aa53
Dustin <dc.christianson@gmail.com>
2021-12-26 16:46:46 -0600

    works, but horribly slow

--- index.js
+++ index.js
@@ -53,39 +53,53 @@ let newStates = [
 let sampleGeneration        = [1,1,0,1,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,1];
 let sampleNextGeneration    = [0,1,1,1,0,0,0,1,1,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,1,1,0];
 
+let output = [];
+
 container.innerText = sampleGeneration;
+let rowCounter = 0;
 
 function step(currentGen, patterns, newStates){
-    let sections = [];
-    let firstSlice = [currentGen[currentGen.length - 1], currentGen[0], currentGen[1]];
-    sections.push(firstSlice);
-    let slice = [];
-    for(let i = 0; i < currentGen.length - 2; i++){
-        slice = [currentGen[i], currentGen[i+1], currentGen[i+2]];
-        sections.push(slice);
-    }
-    let lastSlice = [currentGen[currentGen.length - 2], currentGen[currentGen.length - 1], currentGen[0]];
-    sections.push(lastSlice);
-    //console.log(sections);
-    //console.log(patterns);
 
-    
-    let nextGeneration = [];
-    for(let i = 0; i < sections.length; i++){
-        for(let j = 0; j < patterns.length; j++){
-            if(arraysEqual(sections[i], patterns[j])){
-                nextGeneration.push(newStates[j]);
+    while(rowCounter < 10){
+        let sections = [];
+        let firstSlice = [currentGen[currentGen.length - 1], currentGen[0], currentGen[1]];
+        sections.push(firstSlice);
+        let slice = [];
+        for(let i = 0; i < currentGen.length - 2; i++){
+            slice = [currentGen[i], currentGen[i+1], currentGen[i+2]];
+            sections.push(slice);
+        }
+        let lastSlice = [currentGen[currentGen.length - 2], currentGen[currentGen.length - 1], currentGen[0]];
+        sections.push(lastSlice);
+
+        let nextGeneration = [];
+        for(let i = 0; i < sections.length; i++){
+            for(let j = 0; j < patterns.length; j++){
+                if(arraysEqual(sections[i], patterns[j])){
+                    nextGeneration.push(newStates[j]);
+                }
             }
         }
+
+        //container.innerText += "\n";
+        //container.innerText += nextGeneration;
+        output[rowCounter] = nextGeneration;
+        rowCounter++;
+        step(nextGeneration, patterns, newStates);
     }
-    //console.log(arraysEqual(patterns[0], sections[0]));
-    //console.log(patterns[0] === sections[0])
-    container.innerText += "\n";
-    container.innerText += nextGeneration;
-    return nextGeneration;
+    //return nextGeneration;
 }
 
+step(sampleGeneration, patterns, newStates);
 
+console.log(output);
+
+for(let i = 0; i < output.length; i++){
+    container.innerText += '\n';
+    container.innerText += output[i];
+}
+
+/*
 let secondGeneration = step(sampleGeneration, patterns, newStates);
 let thirdGeneration = step(secondGeneration, patterns, newStates);
 let fourthGeneration = step(thirdGeneration, patterns, newStates);
@@ -94,6 +108,7 @@ console.log(sampleGeneration);
 console.log(secondGeneration);
 
 console.log(`is calculated second generation equal to sample second gen? ${arraysEqual(sampleNextGeneration, secondGeneration)}`)
+*/
 function arraysEqual(a1, a2){
     return JSON.stringify(a1) == JSON.stringify(a2);
 }
